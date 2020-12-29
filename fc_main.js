59b82b366b060cfad1368c3a52f82ba8ef77b8d9
Max MacCamy <maxmac12@gmail.com>
2020-12-28 20:13:08 -0800

    Added 6000fps option for fun

--- fc_main.js
+++ fc_main.js
@@ -2831,7 +2831,7 @@ function autoCookie() {
             }
         }
 
-	var fps_amounts = ['24', '30', '48', '60', '72', '88', '100', '120', '144', '200', '240', '300', '5', '10', '15'];
+	var fps_amounts = ['24', '30', '48', '60', '72', '88', '100', '120', '144', '200', '240', '300', '6000', '5', '10', '15'];
         if (parseInt(fps_amounts[FrozenCookies["fpsModifier"]]) != Game.fps) {
             Game.fps = parseInt(fps_amounts[FrozenCookies["fpsModifier"]]);
         }
